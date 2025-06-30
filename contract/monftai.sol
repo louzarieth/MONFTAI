4d79a4cf6352add7503ce9b2ece71191701fa76d
anass242000 <anass.louzari8@gmail.com>
2025-06-30 11:24:50 +0100

    2

---
+++ contract/monftai.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AINFT is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+
+    constructor() ERC721("AI NFT", "AINFT") Ownable(msg.sender) {}
+
+    function mintNFT(address to, string memory tokenURI) public onlyOwner {
+        _safeMint(to, nextTokenId);
+        _setTokenURI(nextTokenId, tokenURI);
+        nextTokenId++;
+    }
+}
